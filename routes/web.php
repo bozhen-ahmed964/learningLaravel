15f8d1c97a6988fa72f1d615faf59e4eb7f2c71c
bozhen964 <bozhencomputer964@gmail.com>
2024-05-29 22:25:58 +0300

    creating layout + installing tailwindCss

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,22 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+
+Route::get('/service', function () {
+    return view('service');
+});
+
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+
